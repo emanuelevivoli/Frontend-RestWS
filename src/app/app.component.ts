7a5563bfd783cf7caa3b6c06ea0e409b1989f8bb
Emanuele Vivoli <vivoli.emanuele@gmail.com>
2019-08-07 11:44:31 +0200

    sentences are done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,31 +16,24 @@ export class AppComponent {
   operation = ['-', '*'];
 
   name = '';
-  formula = '0 + 0';
-  realResult = 0;
-  modelResult: number;
+  formula = '';
+  realResult = '';
+  modelResult: string;
   score: number;
-
-  numbers = [0, 0];
-  op = 0;
 
   constructor(
     private http: HttpClient,
     ) { }
 
-  generateFormula() {
-    this.numbers[0] = Math.floor(Math.random() * 100);
-    this.numbers[1] = Math.floor(Math.random() * 100);
-    this.op = Math.floor(Math.random() * this.operation.length);
-
-    this.formula = String(this.numbers[0]) + ' ' + this.operation[this.op] + ' ' + String(this.numbers[1]);
-  }
-
   update(data: JSON) {
     console.log(data);
     this.score = data['score'];
-    this.modelResult = data['model_result'];
-    this.realResult = data['user_result'];
+    if (data['model_result'] == true){
+      this.modelResult = 'positive';
+    }else{
+      this.modelResult = 'negative';
+    }
+
   }
 
   getDataUsingPromise(params: HttpParams) {
@@ -52,9 +45,9 @@ export class AppComponent {
     console.log('I will not wait until promise is resolved..');
   }
 
-  async send(name: string, result: number) {
+  async send(name: string, result: string) {
 
-    const params = new HttpParams().set('user', name).set('formula', this.formula).set('result', String(result));
+    const params = new HttpParams().set('user', name).set('sentence', result);
 
     this.getDataUsingPromise(params);
 
